ed149084f95382ae41eaf9f2e32b0efaebc5ae56
Antoan Bekele <antoan.bekele@gmail.com>
2025-10-23 18:05:31 +0100

    Clean up test files

--- test-schema.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
-
-const DiagnosticsInputSchema = z.object({
-    includeEnvironment: z.boolean().optional().describe('Include environment info'),
-});
-
-const json = zodToJsonSchema(DiagnosticsInputSchema, { name: 'input' });
-console.log(JSON.stringify(json, null, 2));
